1a7b95d4270116432f48c7be80580f2557bf4bea
Noor <noor.muradi@protonmail.com>
2022-07-23 15:36:51 +0530

    Initial commit

---
+++ Node2/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+res.send('<h1><center>Nodejs app2 workig !<center></h1>');
+});
+
+
+
+app.listen(5000, () => {
+  console.log('App listening on port 5000!');
+
+
+});
